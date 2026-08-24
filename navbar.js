4503e8869e776edf8b609f83ffe88f84d19ac5b3
istudiosystemrm3-dev <istudio.systemrm3@gmail.com>
2026-08-24 17:00:28 +0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
             <a href="search.html" id="nav-search">ค้นหารุ่น</a>
             <a href="price.html" id="nav-price">ตารางราคาหน้าร้าน</a>
             <a href="barcode-manual.html" id="nav-barcode">บาร์โค้ด สำหรับใช้หน้าร้าน</a>
-            <a href="stock.html" id="nav-stock">สต๊อกสินค้า</a>
             <a href="firmware.html" id="nav-firmware">เฟิร์มแวร์ล่าสุด (IPSW)</a>
             <a href="macos.html" id="nav-macos">💻 macOS Bootable</a>
             <a href="knowledge.html">📚 คลังความรู้ (Knowledge)</a>
